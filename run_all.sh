9f645e10d6b00ffb9bdccdc04760e7d5b496acf7
Lorenzo Bonanni <lorenzopuntobonanni@gmail.com>
2025-03-09 21:21:15 +0100

    added script too run all

---
+++ run_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python3 project.py --dataset="metropt3" --batch_size=10
+python3 project.py --dataset="metropt3" --batch_size=100
+python3 project.py --dataset="metropt3" --batch_size=1000
+python3 project.py --dataset="metropt3" --batch_size=10000
+python3 project.py --dataset="household" --batch_size=10
+python3 project.py --dataset="household" --batch_size=100
+python3 project.py --dataset="household" --batch_size=1000
+python3 project.py --dataset="household" --batch_size=10000
